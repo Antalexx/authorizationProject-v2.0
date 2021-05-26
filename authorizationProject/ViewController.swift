94b24e38dd3875c4621a3a09a895be8c86f248c0
Антоненко Александр <aalexanto@gmail.com>
2021-05-26 19:40:33 +0300

    1 view

--- authorizationProject/ViewController.swift
+++ authorizationProject/ViewController.swift
@@ -8,7 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var forgotYouPassword: UIButton!
+    @IBOutlet weak var forgotYouUserName: UIButton!
+    
+    let user = "shikaka"
+    let pass = "password"
+    
+    @IBAction func login() {
+        let alertLogin = UIAlertController(title: "📛", message: "Ахтунг!! Введи правильно", preferredStyle: .alert)
+        alertLogin.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        
+        
+        if userName.text == user && password.text == pass {
+            //переход в другой экран
+        }else {
+            present(alertLogin, animated: true)
+            userName.text = ""
+            password.text = ""
+            
+        }
+    }
+    
+    
+    @IBAction func fypTouch() {
+        let alertFyp = UIAlertController(title: "Oops!", message: "You Password is \(pass)🤬", preferredStyle: .alert)
+        alertFyp.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alertFyp, animated: true)
+    }
+    
+    
+    @IBAction func fyunTouch() {
+        let alertFyun = UIAlertController(title: "Oops!", message: "You User name is \(user)🤬", preferredStyle: .alert)
+        alertFyun.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alertFyun, animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
